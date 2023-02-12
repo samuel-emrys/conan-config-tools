c1500ad8b20fe519c21f787573eb8fc63d7c9a4e
Samuel Dowling <samuel.dowling@protonmail.com>
2023-02-12 11:39:26 +1030

    Add settings.yml to test resourced for reproducible tests
    * Add settings.yml to test resources to enable tests to be reproducible
      regardless of environment. This removes a dependency on
      $HOME/.conan/settings.yml existing for unit tests to pass.
    * Add mocks to manage setup and teardown of resources to adhere to the
      DRY principle

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -9,12 +9,19 @@ from conan_config_tools import application
 from conan_config_tools.cli import cli
 from click.testing import CliRunner
 
-
-def test_profile_exists_does_not_throw_when_forced(caplog):
-    # cct -v --conan-home . set-profile -f -n test -s build_type=Debug
-    profile_dir = pathlib.Path("profiles")
+@pytest.fixture()
+def mock_conan_home():
+    conan_home = pathlib.Path(".conan")
+    profile_dir = pathlib.Path(conan_home, "profiles")
     profile_dir.mkdir(parents=True, exist_ok=True)
-    profile = pathlib.Path(profile_dir, "test")
+    settings_yml = pathlib.Path(pathlib.Path(__file__).parent.absolute(), "resources", "settings.yml")
+    shutil.copy(src=settings_yml, dst=conan_home)
+    yield conan_home
+    shutil.rmtree(conan_home)
+
+@pytest.fixture()
+def mock_existing_profile(mock_conan_home):
+    profile = pathlib.Path(mock_conan_home, "profiles", "test")
 
     # Dummy profile
     config = configparser.ConfigParser()
@@ -29,13 +36,16 @@ def test_profile_exists_does_not_throw_when_forced(caplog):
     with profile.open("w") as f:
         config.write(f, space_around_delimiters=False)
 
+    yield profile
+
+
+def test_profile_exists_does_not_throw_when_forced(caplog, mock_conan_home, mock_existing_profile):
+    # cct -v --conan-home . set-profile -f -n test -s build_type=Debug
+
     # Invoke the CLI
     runner = CliRunner()
-    result = runner.invoke(cli, ["-v", "--conan-home", ".", "set-profile", "-f", "-n", "test", "-s", "build_type=Debug"])
+    result = runner.invoke(cli, ["-v", "--conan-home", mock_conan_home, "set-profile", "-f", "-n", "test", "-s", "build_type=Debug"])
 
-    # Clean up after test
-    if profile_dir.exists():
-        shutil.rmtree(profile_dir)
     # No error in exit code
     assert result.exit_code == 0
     # No critical error messages
@@ -45,58 +55,45 @@ def test_profile_exists_does_not_throw_when_forced(caplog):
     assert "Profile 'test' already exists! Overwriting." in [r.msg for r in caplog.records]
 
 
-def test_profile_does_not_exist_does_not_throw(caplog):
+def test_profile_does_not_exist_does_not_throw(caplog, mock_conan_home):
     # cct -v --conan-home . set-profile -f -n test -s build_type=Debug
     # Invoke the CLI
     runner = CliRunner()
-    result = runner.invoke(cli, ["-v", "--conan-home", ".", "set-profile", "-n", "test", "-s", "build_type=Debug"])
+    result = runner.invoke(cli, ["-v", "--conan-home", mock_conan_home, "set-profile", "-n", "test", "-s", "build_type=Debug"])
 
-    # Clean up after test
-    profile_dir = pathlib.Path("profiles")
-    if profile_dir.exists():
-        shutil.rmtree(profile_dir)
     assert result.exit_code == 0
     for record in caplog.records:
         assert record.levelname != "CRITICAL"
 
-def test_invalid_setting_is_sanitized_from_profile_when_forced(caplog):
+def test_invalid_setting_is_sanitized_from_profile_when_forced(caplog, mock_conan_home):
     # cct -v --conan-home . set-profile -f -n test -s compiler="Visual Studio" -s compiler.libcxx=libstdc++11
     # Invoke the CLI
     runner = CliRunner()
-    result = runner.invoke(cli, ["-v", "--conan-home", ".", "set-profile", "-f", "-n", "test", "-s", "compiler=Visual Studio", "-s", "compiler.libcxx=libstdc++11"])
+    result = runner.invoke(cli, ["-v", "--conan-home", mock_conan_home, "set-profile", "-f", "-n", "test", "-s", "compiler=Visual Studio", "-s", "compiler.libcxx=libstdc++11"])
 
     assert result.exit_code == 0
     for record in caplog.records:
         assert record.levelname != "CRITICAL"
 
-    profile_dir = pathlib.Path("profiles")
-    profile = pathlib.Path(profile_dir, "test")
+    profile = pathlib.Path(mock_conan_home, "profiles", "test")
     config = configparser.ConfigParser()
     config.read(profile)
 
+    # Profile should not contain compiler.libcxx key
     with pytest.raises(Exception) as exc:
         config.get("settings", "compiler.libcxx")
 
-    # Clean up after test
-    if profile_dir.exists():
-        shutil.rmtree(profile_dir)
 
-def test_invalid_setting_throws(caplog):
+def test_invalid_setting_throws(caplog, mock_conan_home):
     # cct -v --conan-home . set-profile  -n test -s compiler="Visual Studio" -s compiler.libcxx=libstdc++11
     # Invoke the CLI
     runner = CliRunner()
-    result = runner.invoke(cli, ["-v", "--conan-home", ".", "set-profile", "-n", "test", "-s", "compiler=Visual Studio", "-s", "compiler.libcxx=libstdc++11"])
+    result = runner.invoke(cli, ["-v", "--conan-home", mock_conan_home, "set-profile", "-n", "test", "-s", "compiler=Visual Studio", "-s", "compiler.libcxx=libstdc++11"])
 
     assert result.exit_code == 1 # ValueError
     # Invalid configuration should cause a critical error when not forced
     assert "CRITICAL" in [r.levelname for r in caplog.records]
     assert "'compiler.libcxx' is not a valid setting for compiler Visual Studio! Force removal of invalid keys with -f" in [r.msg for r in caplog.records]
-
-    profile_dir = pathlib.Path("profiles")
-    profile = pathlib.Path(profile_dir, "test")
-    # Clean up after test
-    if profile_dir.exists():
-        shutil.rmtree(profile_dir)
 
 # def test_valid_profile_does_not_throw(caplog):
 #     assert False

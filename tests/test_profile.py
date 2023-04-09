0d51a456771f804cefaf1f8499b65e6bb49971c9
Samuel Dowling <samuel.dowling@protonmail.com>
2023-04-09 12:54:31 +0930

    Add conan 2.0 compatibility
    * Improve README to add usage examples
    * Transition to usage of conan as a library to manage the construction
      of a profile and it's validation
    * Modify conan profile validation to include the ability to sanitize
      invalid settings from the profile
    * Update unit tests to reflect new conan cache management
    
    Closes #1.

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -14,8 +14,6 @@ def mock_conan_home():
     conan_home = pathlib.Path(".conan")
     profile_dir = pathlib.Path(conan_home, "profiles")
     profile_dir.mkdir(parents=True, exist_ok=True)
-    settings_yml = pathlib.Path(pathlib.Path(__file__).parent.absolute(), "resources", "settings.yml")
-    shutil.copy(src=settings_yml, dst=conan_home)
     yield conan_home
     shutil.rmtree(conan_home)
 
@@ -27,9 +25,7 @@ def mock_existing_profile(mock_conan_home):
     config = configparser.ConfigParser()
     config["settings"] = {
         "os": platform.system(),
-        "os_build": platform.system(),
-        "arch": application._get_arch(),
-        "arch_build": application._get_arch(),
+        "arch": platform.machine() if platform.machine() != "AMD64" else "x86_64"
     }
 
     # Populate an existing profile
@@ -66,10 +62,10 @@ def test_profile_does_not_exist_does_not_throw(caplog, mock_conan_home):
         assert record.levelname != "CRITICAL"
 
 def test_invalid_setting_is_sanitized_from_profile_when_forced(caplog, mock_conan_home):
-    # cct -v --conan-home . set-profile -f -n test -s compiler="Visual Studio" -s compiler.libcxx=""
+    # cct -v --conan-home . set-profile -f -n test -s compiler="msvc -s compiler.libcxx=""
     # Invoke the CLI
     runner = CliRunner()
-    result = runner.invoke(cli, ["-v", "--conan-home", mock_conan_home, "set-profile", "-f", "-n", "test", "-s", "compiler=Visual Studio", "-s", "compiler.libcxx="])
+    result = runner.invoke(cli, ["-v", "--conan-home", mock_conan_home, "set-profile", "-f", "-n", "test", "-s", "compiler=msvc", "-s", "compiler.libcxx="])
 
     assert result.exit_code == 0
     for record in caplog.records:
@@ -88,12 +84,12 @@ def test_invalid_setting_throws(caplog, mock_conan_home):
     # cct -v --conan-home . set-profile  -n test -s compiler="Visual Studio" -s compiler.libcxx=libstdc++11
     # Invoke the CLI
     runner = CliRunner()
-    result = runner.invoke(cli, ["-v", "--conan-home", mock_conan_home, "set-profile", "-n", "test", "-s", "compiler=Visual Studio", "-s", "compiler.libcxx=libstdc++11"])
+    result = runner.invoke(cli, ["-v", "--conan-home", mock_conan_home, "set-profile", "-n", "test", "-s", "compiler=msvc", "-s", "compiler.libcxx=libstdc++11"])
 
     assert result.exit_code == 1 # ValueError
     # Invalid configuration should cause a critical error when not forced
     assert "CRITICAL" in [r.levelname for r in caplog.records]
-    assert "'compiler.libcxx' is not a valid setting for compiler Visual Studio! Force removal of invalid keys with -f" in [r.msg for r in caplog.records]
+    assert "'settings.compiler.libcxx' doesn't exist for 'msvc'" in "\n".join([str(r.msg) for r in caplog.records])
 
 # def test_valid_profile_does_not_throw(caplog):
 #     assert False
